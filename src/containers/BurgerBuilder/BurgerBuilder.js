9fba064bc50179f55752209a456868b393f83eaa
Deepeshbaid <deepesh.baid@gmail.com>
2019-05-05 18:22:53 +0530

    added build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component{
     state={
@@ -17,7 +18,7 @@ class BurgerBuilder extends Component{
         return(
             <Auxi>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Burger build controls</div>
+                <BuildControls/>
             </Auxi>
 
         );
